2f109662a3b6880b81bfe206c2f5a3b5400d8d62
Granz Fisherman <granz.fisherman@gmail.com>
2024-10-13 16:50:05 +0900

    Add dev_urandom

--- fquuid_random_unix.hpp
+++ fquuid_random_unix.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include <cstdint>
+#include <cstdio>
 #include <cstring>
 #include <limits>
 #include <stdexcept>
@@ -29,6 +30,65 @@ namespace fquuid
                 return r;
             }
         }
+    };
+
+    template <class ResultType>
+    class uuid_dev_urandom
+    {
+        FILE *file_;
+
+    public:
+        using result_type = ResultType;
+        static constexpr result_type min() { return std::numeric_limits<result_type>::min(); }
+        static constexpr result_type max() { return std::numeric_limits<result_type>::max(); }
+
+        uuid_dev_urandom() {
+            file_ = nullptr;
+        }
+
+        ~uuid_dev_urandom() {
+            close();
+        }
+
+        void open() {
+            if (file_ != nullptr) return;
+
+            file_ = fopen("/dev/urandom", "r");
+            if (file_ == nullptr)
+                throw std::runtime_error("uuid_dev_urandom: can't open file");
+        }
+
+        void close() {
+            if (file_ != nullptr) {
+                fclose(file_);
+                file_ = nullptr;
+            }
+        }
+
+        result_type operator ()() {
+            if (file_ == nullptr)
+                open();
+
+            result_type r;
+            auto ret = fread(&r, sizeof(r), 1, file_);
+            if (ret == 0)
+                throw std::runtime_error("uuid_dev_urandom: read error");
+            return r;
+        }
+
+        uuid_dev_urandom(uuid_dev_urandom&& r) {
+            file_ = r.file_;
+            r.file_ = nullptr;
+        }
+
+        uuid_dev_urandom& operator =(uuid_dev_urandom&& r) {
+            close();
+            file_ = r.file_;
+            r.file_ = nullptr;
+        }
+
+        uuid_dev_urandom(const uuid_dev_urandom&) = delete;
+        uuid_dev_urandom& operator =(const uuid_dev_urandom&) = delete;
     };
 
     using uuid_random = uuid_sys_getrandom<uint64_t>;
